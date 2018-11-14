33dfd109d890792db69712e17ce058f448bf6457
Harrolee <Harrolee@users.noreply.github.com>
2018-11-14 14:51:44 -0800

    Spatial menu
    Began interactive menu.

---
+++ Assets/CueGalleryMenu.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CueGalleryMenu : MonoBehaviour {
+
+    public float howHigh;
+    GameObject menuManager;
+    float currPos;
+
+    void Start()
+    {
+        menuManager = GameObject.FindWithTag("SceneManager");
+        currPos = transform.position.y;
+        menuManager.GetComponent<MenuManager>().OpenGalleryStand();
+        StartCoroutine(RaiseSelf(howHigh + currPos));
+
+    }
+
+
+
+    void OnColliderEnter()
+    {
+        menuManager.GetComponent<MenuManager>().OpenGalleryStand();
+        StartCoroutine(RaiseSelf(howHigh));
+    }
+    
+
+    IEnumerator RaiseSelf(float end)
+    {
+        for (int i = 0; i < 40 ; i++)
+        {
+            currPos += Mathf.Lerp(currPos, end, Time.deltaTime);
+            Debug.Log("currPos: " + currPos);
+            transform.position += new Vector3(0, currPos, 0);
+            yield return null;
+        }
+ 
+    }
+
+}
---
+++ Assets/MenuManager.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuManager : MonoBehaviour {
+
+    public List<GameObject> Galleries;
+    public List<GameObject> GalleryStands;
+    public List<GameObject> Sculptures;
+    //rotate gallery type
+    //rotate gallery selection menu
+
+
+        //test framerate
+    void Start()
+    {
+        OpenGalleryStand();
+
+    }
+
+
+
+
+    public void OpenGalleryStand()
+    {
+        print("opening Gallery");
+        StartCoroutine(RotateY(GalleryStands[0], 180));
+        StartCoroutine(RotateY(GalleryStands[1], 180));
+
+        for (int i = 0; i < Sculptures.Count; i++)
+        {
+            StartCoroutine(Spin(Sculptures[i]));
+        }
+    }
+
+
+
+
+    IEnumerator RotateY(GameObject go, float angle)
+    {
+        for (int i = 0; i < angle; i++)
+        {
+        go.transform.RotateAround(go.transform.position, Vector3.down, 1);
+        yield return null;
+        }
+    }
+
+
+    IEnumerator Spin(GameObject go)
+    {
+        go.transform.RotateAround(go.transform.position, Vector3.down, 1);
+        yield return null;
+        StartCoroutine(Spin(go));
+    }
+
+}
