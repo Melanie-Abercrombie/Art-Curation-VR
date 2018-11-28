cb8a8d106467844a509d25e263759ec538546904
Harrolee <Harrolee@users.noreply.github.com>
2018-11-27 17:05:23 -0800

    InvDesign
    Laptop Dying

---
+++ Assets/PlayerManager.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerManager : MonoBehaviour {
+
+//reference object to be rotated
+
+
+    void RotateInventory(float inertiaApplied)
+    {
+        
+    }
+
+}
