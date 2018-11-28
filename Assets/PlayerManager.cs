3954c8e696505bf787c55eae396ad7916600f827
harrolee <>
2018-11-27 21:03:28 -0800

    Inventory design
    Left Trigger spins inventory. On Start, list<> of sculptures are assigned to the inventory. I don't yet know how to OH WAIT maybe I make an enum with the number of possibilities equal to the number of sculptures. Then, when the selection point matches the range, the enum points the selector to the gameobject selected and spawn it for the player.

--- Assets/PlayerManager.cs
+++ Assets/PlayerManager.cs
@@ -1,15 +1,57 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using OVRTouchSample;
+using System;
 
 public class PlayerManager : MonoBehaviour {
 
-//reference object to be rotated
+    public GameObject _Inventory;  //reference object to be rotated
+    Vector2 thumbstick;
+
+    List<GameObject> sculptures;
+   void Start()
+    {
+        PopulateInventory();
+    }
 
 
+    void Update()
+    {
+
+       if(OVRInput.Get(OVRInput.Axis2D.PrimaryThumbstick) != Vector2.zero)
+        {
+            _Inventory.GetComponent<MeshRenderer>().enabled = true;
+            thumbstick = OVRInput.Get(OVRInput.Axis2D.PrimaryThumbstick);
+            RotateInventory(thumbstick.y);
+        }
+        thumbstick = Vector2.zero;
+
+    }
+
+
+    void PopulateInventory()
+    {
+        sculptures = GetComponent<MenuManager>().Sculptures;
+        for (int i = 0; i < sculptures.Count; i++)
+        {
+           
+        }
+    }
+
+
+
+    Vector3 z;
     void RotateInventory(float inertiaApplied)
     {
-        
+        z.z = inertiaApplied;
+        _Inventory.GetComponent<Rigidbody>().AddTorque(z);
     }
+
+    
+   // enum slot { One, Two, Three, Four, Five, Six};
+
+
+    
 
 }
